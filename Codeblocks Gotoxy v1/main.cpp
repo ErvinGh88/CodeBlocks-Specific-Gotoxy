b2700383c2c1f10cb955e8ed59264c766e7c36a6
Ervin <114607931+ErvinGh88@users.noreply.github.com>
2023-03-01 17:21:57 +0330

    Add files via upload

---
+++ Codeblocks Gotoxy v1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void gotoxy(int x, int y){
+    static HANDLE h = NULL;
+    if (!h)
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD c = { x , y };
+    SetConsoleCursorPosition( h , c );
+}
+
+int main()
+{
+    // the upper function is the mane function
+    /* this is codeblocks specific gotoxy (*console app) that you can use it for free:) <3 */
+
+    // template: ->  gotoxy( "colse/mid/far" , "line" );
+    // test it to better know :)
+
+    //test 1
+    gotoxy(50,2);
+    cout<<"test 1";
+    // tricks ->
+                // put 1st num "50" for put in middle
+                // 2nd num is line (here i put "test" in line 2)
+
+
+
+    //test 2
+    gotoxy(80,3);
+    cout<<"test 2";
+    // tricks ->
+                // put 1st num "80" for put in middle (*but in full screen of console)
+                // 2nd num is line (here i put "test" in line 3)
+
+    return 0;
+}
